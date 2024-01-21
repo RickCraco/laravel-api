4cf8aa4905db1a10d99a72b2e08bf6038107da7d
RickCraco <riccardocracolici43@gmail.com>
2024-01-21 11:33:52 +0100

    created API route for projects

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+
 Route::get('/projects', [ProjectController::class, 'index']);
 Route::get('/projects/{project}', [ProjectController::class, 'show']);
